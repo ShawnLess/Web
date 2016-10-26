e79ba5c2b73738b9b0b5824103dd188b3e99f202
shawnless <shawnless.xie@gmail.com>
2016-10-26 14:56:17 -0700

    added the mobile.css for mobile view

--- app/controllers/baby.php
+++ app/controllers/baby.php
@@ -7,12 +7,14 @@ class Baby extends Controller {
 	}	
 
 	public function baby(){
+/*
         if( Session::get('baby') == true){
 		        Session::set('baby', false);
         }else{
             Session::set('baby', true);
         }
-//      Session::set('baby', true);
+*/
+        Session::set('baby', true);
 
         url::redirect('entries');
 	}
